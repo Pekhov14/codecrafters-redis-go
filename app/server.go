e070796271a69e7844cf428b51ab47ad1e4e252b
pekhov <pehovanton21@gmail.com>
2024-04-08 21:05:00 +0300

    Response for ping request

--- app/server.go
+++ app/server.go
@@ -2,19 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
+const MAX_BUF_SIZE = 1024
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer listener.Close()
+
+	for {
+		// Block until we receive an incoming connection
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			continue
+		}
+
+		// Handle client connection
+		go handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	defer conn.Close()
+
+	// Read data
+	buf := make([]byte, MAX_BUF_SIZE)
+	n, err := conn.Read(buf) // n
+	if err != nil {
+		return
+	}
+
+	log.Println("Received data", buf[:n])
+
+	message := "+PONG\r\n"
+	conn.Write([]byte(message))
+
+	// Write the same data back
+	// conn.Write(buf[:n])
 }
